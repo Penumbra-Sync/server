ccb9478f704b8d172af96dd9fe573e780570d7dc
Stanley Dimant <root.darkarchon@outlook.com>
2022-08-03 23:02:42 +0200

    remove throttling

--- MareSynchronosServer/MareSynchronosServer/Startup.cs
+++ MareSynchronosServer/MareSynchronosServer/Startup.cs
@@ -18,7 +18,6 @@ using Microsoft.Extensions.FileProviders;
 using Microsoft.AspNetCore.Authorization;
 using MareSynchronosServer.Discord;
 using AspNetCoreRateLimit;
-using MareSynchronosServer.Throttling;
 
 namespace MareSynchronosServer
 {
