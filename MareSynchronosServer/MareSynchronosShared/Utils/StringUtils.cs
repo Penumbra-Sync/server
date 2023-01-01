f8c9d1e64f17603d5a169d6708ff069d0fa99cd9
rootdarkarchon <root.darkarchon@outlook.com>
2023-01-01 17:32:22 +0100

    shuffle cdn shard configs to allow for multiple file shards to handle one file, randomly distributing

--- MareSynchronosServer/MareSynchronosShared/Utils/StringUtils.cs
+++ MareSynchronosServer/MareSynchronosShared/Utils/StringUtils.cs
@@ -29,3 +29,21 @@ public static class StringUtils
         return BitConverter.ToString(sha256.ComputeHash(Encoding.UTF8.GetBytes(input))).Replace("-", "", StringComparison.OrdinalIgnoreCase);
     }
 }
+
+public static class ListUtils
+{
+    private static Random rng = new();
+
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
