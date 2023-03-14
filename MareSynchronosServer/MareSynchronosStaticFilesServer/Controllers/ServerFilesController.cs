26094bc6dd2a8a9a0242dba4e0bb66b595ae9f06
rootdarkarchon <root.darkarchon@outlook.com>
2023-03-14 16:48:20 +0100

    rework file upload

--- MareSynchronosServer/MareSynchronosStaticFilesServer/Controllers/ServerFilesController.cs
+++ MareSynchronosServer/MareSynchronosStaticFilesServer/Controllers/ServerFilesController.cs
@@ -1,6 +1,8 @@
 ﻿using LZ4;
 using MareSynchronos.API.Dto.Files;
 using MareSynchronos.API.Routes;
+using MareSynchronos.API.SignalR;
+using MareSynchronosServer.Hubs;
 using MareSynchronosShared.Data;
 using MareSynchronosShared.Metrics;
 using MareSynchronosShared.Models;
@@ -10,6 +12,7 @@ using MareSynchronosStaticFilesServer.Services;
 using MareSynchronosStaticFilesServer.Utils;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Concurrent;
 using System.Security.Cryptography;
@@ -21,33 +24,25 @@ namespace MareSynchronosStaticFilesServer.Controllers;
 [Route(MareFiles.ServerFiles)]
 public class ServerFilesController : ControllerBase
 {
+    private static readonly ConcurrentDictionary<string, SemaphoreSlim> _fileUploadLocks = new(StringComparer.Ordinal);
+    private readonly string _basePath;
     private readonly CachedFileProvider _cachedFileProvider;
     private readonly IConfigurationService<StaticFilesServerConfiguration> _configuration;
+    private readonly IHubContext<MareHub> _hubContext;
     private readonly MareDbContext _mareDbContext;
     private readonly MareMetrics _metricsClient;
-    private readonly ConcurrentDictionary<string, SemaphoreSlim> _fileUploadLocks = new(StringComparer.Ordinal);
-    private readonly string _basePath;
 
     public ServerFilesController(ILogger<ServerFilesController> logger, CachedFileProvider cachedFileProvider,
-        IConfigurationService<StaticFilesServerConfiguration> configuration, MareDbContext mareDbContext, MareMetrics metricsClient) : base(logger)
+        IConfigurationService<StaticFilesServerConfiguration> configuration,
+        IHubContext<MareSynchronosServer.Hubs.MareHub> hubContext,
+        MareDbContext mareDbContext, MareMetrics metricsClient) : base(logger)
     {
         _basePath = configuration.GetValue<string>(nameof(StaticFilesServerConfiguration.CacheDirectory));
         _cachedFileProvider = cachedFileProvider;
         _configuration = configuration;
+        _hubContext = hubContext;
         _mareDbContext = mareDbContext;
         _metricsClient = metricsClient;
-    }
-
-    [HttpGet(MareFiles.ServerFiles_Get + "/{fileId}")]
-    [Authorize(Policy = "Internal")]
-    public IActionResult GetFile(string fileId)
-    {
-        _logger.LogInformation($"GetFile:{MareUser}:{fileId}");
-
-        var fs = _cachedFileProvider.GetLocalFileStream(fileId);
-        if (fs == null) return NotFound();
-
-        return File(fs, "application/octet-stream");
     }
 
     [HttpPost(MareFiles.ServerFiles_DeleteAll)]
@@ -71,42 +66,6 @@ public class ServerFilesController : ControllerBase
         await _mareDbContext.SaveChangesAsync().ConfigureAwait(false);
 
         return Ok();
-    }
-
-    [HttpPost(MareFiles.ServerFiles_FilesSend)]
-    public async Task<IActionResult> FilesSend([FromBody] List<string> fileListHashes)
-    {
-        var userSentHashes = new HashSet<string>(fileListHashes.Distinct(StringComparer.Ordinal).Select(s => string.Concat(s.Where(c => char.IsLetterOrDigit(c)))), StringComparer.Ordinal);
-        var notCoveredFiles = new Dictionary<string, UploadFileDto>(StringComparer.Ordinal);
-        var forbiddenFiles = await _mareDbContext.ForbiddenUploadEntries.AsNoTracking().Where(f => userSentHashes.Contains(f.Hash)).AsNoTracking().ToDictionaryAsync(f => f.Hash, f => f).ConfigureAwait(false);
-        var existingFiles = await _mareDbContext.Files.AsNoTracking().Where(f => userSentHashes.Contains(f.Hash)).AsNoTracking().ToDictionaryAsync(f => f.Hash, f => f).ConfigureAwait(false);
-
-        List<FileCache> fileCachesToUpload = new();
-        foreach (var hash in userSentHashes)
-        {
-            // Skip empty file hashes, duplicate file hashes, forbidden file hashes and existing file hashes
-            if (string.IsNullOrEmpty(hash)) { continue; }
-            if (notCoveredFiles.ContainsKey(hash)) { continue; }
-            if (forbiddenFiles.ContainsKey(hash))
-            {
-                notCoveredFiles[hash] = new UploadFileDto()
-                {
-                    ForbiddenBy = forbiddenFiles[hash].ForbiddenBy,
-                    Hash = hash,
-                    IsForbidden = true,
-                };
-
-                continue;
-            }
-            if (existingFiles.TryGetValue(hash, out var file) && file.Uploaded) { continue; }
-
-            notCoveredFiles[hash] = new UploadFileDto()
-            {
-                Hash = hash,
-            };
-        }
-
-        return Ok(JsonSerializer.Serialize(notCoveredFiles.Values.ToList()));
     }
 
     [HttpGet(MareFiles.ServerFiles_GetSizes)]
@@ -142,6 +101,60 @@ public class ServerFilesController : ControllerBase
         return Ok(JsonSerializer.Serialize(response));
     }
 
+    [HttpPost(MareFiles.ServerFiles_FilesSend)]
+    public async Task<IActionResult> FilesSend([FromBody] FilesSendDto filesSendDto)
+    {
+        var userSentHashes = new HashSet<string>(filesSendDto.FileHashes.Distinct(StringComparer.Ordinal).Select(s => string.Concat(s.Where(c => char.IsLetterOrDigit(c)))), StringComparer.Ordinal);
+        var notCoveredFiles = new Dictionary<string, UploadFileDto>(StringComparer.Ordinal);
+        var forbiddenFiles = await _mareDbContext.ForbiddenUploadEntries.AsNoTracking().Where(f => userSentHashes.Contains(f.Hash)).AsNoTracking().ToDictionaryAsync(f => f.Hash, f => f).ConfigureAwait(false);
+        var existingFiles = await _mareDbContext.Files.AsNoTracking().Where(f => userSentHashes.Contains(f.Hash)).AsNoTracking().ToDictionaryAsync(f => f.Hash, f => f).ConfigureAwait(false);
+
+        List<FileCache> fileCachesToUpload = new();
+        foreach (var hash in userSentHashes)
+        {
+            // Skip empty file hashes, duplicate file hashes, forbidden file hashes and existing file hashes
+            if (string.IsNullOrEmpty(hash)) { continue; }
+            if (notCoveredFiles.ContainsKey(hash)) { continue; }
+            if (forbiddenFiles.ContainsKey(hash))
+            {
+                notCoveredFiles[hash] = new UploadFileDto()
+                {
+                    ForbiddenBy = forbiddenFiles[hash].ForbiddenBy,
+                    Hash = hash,
+                    IsForbidden = true,
+                };
+
+                continue;
+            }
+            if (existingFiles.TryGetValue(hash, out var file) && file.Uploaded) { continue; }
+
+            notCoveredFiles[hash] = new UploadFileDto()
+            {
+                Hash = hash,
+            };
+        }
+
+        if (notCoveredFiles.Any())
+        {
+            await _hubContext.Clients.Users(filesSendDto.UIDs).SendAsync(nameof(IMareHub.Client_UserReceiveUploadStatus), new MareSynchronos.API.Dto.User.UserDto(new(MareUser)))
+                .ConfigureAwait(false);
+        }
+
+        return Ok(JsonSerializer.Serialize(notCoveredFiles.Values.ToList()));
+    }
+
+    [HttpGet(MareFiles.ServerFiles_Get + "/{fileId}")]
+    [Authorize(Policy = "Internal")]
+    public IActionResult GetFile(string fileId)
+    {
+        _logger.LogInformation($"GetFile:{MareUser}:{fileId}");
+
+        var fs = _cachedFileProvider.GetLocalFileStream(fileId);
+        if (fs == null) return NotFound();
+
+        return File(fs, "application/octet-stream");
+    }
+
     [HttpPost(MareFiles.ServerFiles_Upload + "/{hash}")]
     [RequestSizeLimit(200 * 1024 * 1024)]
     public async Task<IActionResult> UploadFile(string hash, CancellationToken requestAborted)
@@ -149,21 +162,14 @@ public class ServerFilesController : ControllerBase
         _logger.LogInformation("{user} uploading file {file}", MareUser, hash);
         bool initiated = false;
         hash = hash.ToUpperInvariant();
-        var existingFile = await _mareDbContext.Files.SingleOrDefaultAsync(f => f.Hash == hash && f.Uploaded);
+        var existingFile = await _mareDbContext.Files.SingleOrDefaultAsync(f => f.Hash == hash);
         if (existingFile != null) return Ok();
 
         if (!_fileUploadLocks.TryGetValue(hash, out var fileLock))
         {
             initiated = true;
             _fileUploadLocks[hash] = fileLock = new SemaphoreSlim(1);
-            await fileLock.WaitAsync(requestAborted);
-            await _mareDbContext.Files.AddAsync(new FileCache()
-            {
-                Hash = hash,
-                UploadDate = DateTime.UtcNow,
-                UploaderUID = MareUser
-            }).ConfigureAwait(false);
-            await _mareDbContext.SaveChangesAsync().ConfigureAwait(false);
+            await fileLock.WaitAsync(requestAborted).ConfigureAwait(false);
         }
 
         if (!initiated)
@@ -172,23 +178,15 @@ public class ServerFilesController : ControllerBase
             {
                 await fileLock.WaitAsync(requestAborted).ConfigureAwait(false);
                 var file = await _mareDbContext.Files.SingleOrDefaultAsync(c => c.Hash == hash).ConfigureAwait(false);
-                if (file == null)
+                if (file != null)
                 {
-                    await _mareDbContext.Files.AddAsync(new FileCache()
-                    {
-                        Hash = hash,
-                        UploadDate = DateTime.UtcNow,
-                        UploaderUID = MareUser
-                    }).ConfigureAwait(false);
-                    await _mareDbContext.SaveChangesAsync().ConfigureAwait(false);
-                }
-                else
-                {
+                    fileLock.Release();
                     return Ok();
                 }
             }
             catch (OperationCanceledException)
             {
+                fileLock.Release();
                 return Ok();
             }
         }
@@ -217,9 +215,14 @@ public class ServerFilesController : ControllerBase
             await compressedFileStream.CopyToAsync(fileStream).ConfigureAwait(false);
 
             // update on db
-            var file = await _mareDbContext.Files.SingleAsync(f => f.Hash == hash).ConfigureAwait(false);
-            file.Size = compressedFileStream.Length;
-            file.Uploaded = true;
+            await _mareDbContext.Files.AddAsync(new FileCache()
+            {
+                Hash = hash,
+                UploadDate = DateTime.UtcNow,
+                UploaderUID = MareUser,
+                Size = compressedFileStream.Length,
+                Uploaded = true
+            }).ConfigureAwait(false);
             await _mareDbContext.SaveChangesAsync().ConfigureAwait(false);
 
             _metricsClient.IncGauge(MetricsAPI.GaugeFilesTotal, 1);
@@ -230,13 +233,12 @@ public class ServerFilesController : ControllerBase
         catch (Exception e)
         {
             _logger.LogError(e, "Error during file upload");
-            _mareDbContext.Files.Remove(_mareDbContext.Files.SingleOrDefault(f => f.Hash == hash));
-            await _mareDbContext.SaveChangesAsync();
             return BadRequest();
         }
         finally
         {
             fileLock.Release();
+            _fileUploadLocks.TryRemove(hash, out _);
         }
 
         return Ok();
