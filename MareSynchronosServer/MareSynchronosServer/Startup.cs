8ad4205a4a0e0cb8046feff563f0a5fd17d6008f
rootdarkarchon <root.darkarchon@outlook.com>
2023-10-17 21:46:57 +0200

    add command timeout due to migration

--- MareSynchronosServer/MareSynchronosServer/Startup.cs
+++ MareSynchronosServer/MareSynchronosServer/Startup.cs
@@ -258,6 +258,7 @@ public class Startup
             {
                 builder.MigrationsHistoryTable("_efmigrationshistory", "public");
                 builder.MigrationsAssembly("MareSynchronosShared");
+                builder.CommandTimeout(5 * 60);
             }).UseSnakeCaseNamingConvention();
             options.EnableThreadSafetyChecks(false);
         });
