cf3c41bf23592fc9f36fd1c114154a9427db6df1
rootdarkarchon <root.darkarchon@outlook.com>
2024-02-20 00:44:51 +0100

    remove old crap

---
+++ MareSynchronosServer/MareSynchronosServices/Discord/LodestoneModal.cs
@@ -0,0 +1,15 @@
+﻿using Discord;
+using Discord.Interactions;
+
+namespace MareSynchronosServices.Discord;
+
+// todo: remove all this crap at some point
+
+public class LodestoneModal : IModal
+{
+    public string Title => "Verify with Lodestone";
+
+    [InputLabel("Enter the Lodestone URL of your Character")]
+    [ModalTextInput("lodestone_url", TextInputStyle.Short, "https://*.finalfantasyxiv.com/lodestone/character/<CHARACTERID>/")]
+    public string LodestoneUrl { get; set; }
+}
