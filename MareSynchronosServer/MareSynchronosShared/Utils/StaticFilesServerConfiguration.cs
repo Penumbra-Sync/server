24db7aec820b054048730fe431340b247b393327
Stanley Dimant <root.darkarchon@outlook.com>
2024-05-03 19:01:06 +0200

    cold storage retention should be days

--- MareSynchronosServer/MareSynchronosShared/Utils/StaticFilesServerConfiguration.cs
+++ MareSynchronosServer/MareSynchronosShared/Utils/StaticFilesServerConfiguration.cs
@@ -20,7 +20,7 @@ public class StaticFilesServerConfiguration : MareConfigurationBase
     public bool UseColdStorage { get; set; } = false;
     public string? ColdStorageDirectory { get; set; } = null;
     public double ColdStorageSizeHardLimitInGiB { get; set; } = -1;
-    public double ColdStorageUnusedFileRetentionPeriodInDays { get; set; } = 30;
+    public int ColdStorageUnusedFileRetentionPeriodInDays { get; set; } = 30;
     [RemoteConfiguration]
     public Uri CdnFullUrl { get; set; } = null;
     [RemoteConfiguration]
