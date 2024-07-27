30f003828e43cd6f35a51fdf120edbd7569b0fd9
Stanley Dimant <root.darkarchon@outlook.com>
2024-07-27 17:36:44 +0200

    reduce secondary uids to 20

--- MareSynchronosServer/MareSynchronosServices/Discord/MareWizardModule.Secondary.cs
+++ MareSynchronosServer/MareSynchronosServices/Discord/MareWizardModule.Secondary.cs
@@ -26,10 +26,10 @@ public partial class MareWizardModule
             + "Secondary UIDs act as completely separate Mare accounts with their own pair list, joined syncshells, UID and so on." + Environment.NewLine
             + "Use this to create UIDs if you want to use Mare on two separate game instances at once or keep your alts private." + Environment.NewLine + Environment.NewLine
             + "__Note:__ Creating a Secondary UID is _not_ necessary to use Mare for alts." + Environment.NewLine + Environment.NewLine
-            + $"You currently have {secondaryUids} Secondary UIDs out of a maximum of 25.");
+            + $"You currently have {secondaryUids} Secondary UIDs out of a maximum of 20.");
         ComponentBuilder cb = new();
         AddHome(cb);
-        cb.WithButton("Create Secondary UID", "wizard-secondary-create:" + primaryUID, ButtonStyle.Primary, emote: new Emoji("2️⃣"), disabled: secondaryUids >= 25);
+        cb.WithButton("Create Secondary UID", "wizard-secondary-create:" + primaryUID, ButtonStyle.Primary, emote: new Emoji("2️⃣"), disabled: secondaryUids >= 20);
         await ModifyInteraction(eb, cb).ConfigureAwait(false);
     }
 
